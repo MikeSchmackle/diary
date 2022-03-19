cdc0a0eeb3369d6ac528f9eb979004cad423e49e
mikeschmackle <mike.schmackle@gmail.com>
2022-03-19 12:40:18 -0500

    commit directory

---
+++ diary_webapp/lib/screens/main_page.dart
@@ -0,0 +1,177 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String? _dropDownText;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        toolbarHeight: 100,
+        elevation: 4,
+        title: Row(
+          children: [
+            Text(
+              'Diary',
+              style: TextStyle(fontSize: 39, color: Colors.blueGrey.shade100),
+            ),
+            Text(
+              'Book',
+              style: TextStyle(fontSize: 39, color: Colors.green),
+            )
+          ],
+        ),
+        actions: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButton<String>(
+                  items: <String>['Latest', 'Earliest'].map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: TextStyle(color: Colors.grey),
+                        ));
+                  }).toList(),
+                  hint: (_dropDownText == null)
+                      ? Text('Select')
+                      : Text(_dropDownText!),
+                  onChanged: (value) {
+                    if (value == 'Latest') {
+                      setState(() {
+                        _dropDownText = value;
+                      });
+                    } else if (value == 'Earliest') {
+                      setState(() {
+                        _dropDownText = value;
+                      });
+                    }
+                  },
+                ),
+              ),
+              //TODO: create profile
+              Container(
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Expanded(
+                            child: InkWell(
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage: NetworkImage(
+                                'https://picsum.photos/200/300.jpg'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                        )),
+                        Text(
+                          'Schmackle',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.logout_outlined,
+                            size: 19, color: Colors.red))
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      body: Row(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: Border(
+                        right: BorderSide(width: 0.4, color: Colors.blueGrey))),
+                //color: Colors.green,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SfDateRangePicker(
+                        onSelectionChanged: (DateRangePickerSelection) {
+                          print(DateRangePickerSelection.value.toString());
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(38.0),
+                      child: Card(
+                        elevation: 4,
+                        child: TextButton.icon(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.add,
+                              size: 40,
+                              color: Colors.greenAccent,
+                            ),
+                            label: Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Add New',
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                              ),
+                            )),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Expanded(
+                      child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Card(
+                          elevation: 4.0,
+                          child: ListTile(
+                            title: Text("Hello"),
+                          ),
+                        ),
+                      );
+                    },
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Add',
+          child: Icon(
+            Icons.add,
+          )),
+    );
+  }
+}
